38b7b3245709f30782b90fa79cf8202ec0eef547
Brijesh Singh <snghbrjsh@gmail.com>
2017-03-27 21:04:53 +0530

    just for fun

--- nodejs/algorithms/bubble-sort.js
+++ nodejs/algorithms/bubble-sort.js
@@ -1,4 +1,4 @@
-function bSort(arr, order) {
+function bSort(arr, isAsc) {
 	for(var i=0; i<arr.length-1; i++) {
 		for(var j=0; j<arr.length-i-1; j++) {
 			if(arr[j] > arr[j+1]){
@@ -14,4 +14,4 @@ function bSort(arr, order) {
 var a = [8, 10, 5, 1, 4, 9, 3, 2, 6, 7, 13, 11, 12, -1, 0];
 
 console.log('arr: ' + a);
-console.log('bSort: ' + bSort(a));
+console.log('bSort: ' + bSort(a, false));
