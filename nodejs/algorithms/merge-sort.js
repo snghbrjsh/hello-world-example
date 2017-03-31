5c31bfcda613eea66f4a8d51892edccb01eaf5b1
Brijesh Singh <snghbrjsh@gmail.com>
2017-03-31 15:49:01 +0530

    add "desc sort" capability

--- nodejs/algorithms/merge-sort.js
+++ nodejs/algorithms/merge-sort.js
@@ -1,36 +1,51 @@
-function mSort(arr) {
+function mSort(arr, asc) {
+
+    /*
+    *   if 'asc' is true or not supplied sort in ascending order; descending otherwise
+    */
+    asc = (typeof asc !== 'boolean') ? true : asc ;
+
+    var leftHalf, rightHalf, mergedArray;
+
     if(arr.length < 2 ) {
         return arr;
     } else {
-        var leftHalf = arr.slice(0, arr.length/2);
-        var rightHalf = arr.slice(arr.length/2, arr.length);
+        leftHalf = arr.slice(0, arr.length/2);
+        rightHalf = arr.slice(arr.length/2, arr.length);
 
-        return mergeElements(mSort(leftHalf), mSort(rightHalf));
+        mergedArray = mergeElements( mSort(leftHalf, asc), mSort(rightHalf, asc) );
+        return mergedArray;
     }
 
     function mergeElements(a, b) {
         var result = [];
-        while(a.length && b.length) {
-            if(a[0] <= b[0]) {
-                result.push(a.shift());
-            } else {
-                result.push(b.shift());
-            }
+
+        function compare(x,y){
+            return (x <= y);
         }
 
-        while(b.length) {
-            result.push(b.shift());
+        while(a.length && b.length) {
+            if(asc){
+                compare(a[0], b[0]) ? result.push(a.shift()) : result.push(b.shift());
+            } else {
+                compare(a[0], b[0]) ? result.push(b.shift()) : result.push(a.shift()); 
+            }
         }
 
         while(a.length) {
             result.push(a.shift());
         }
 
+        while(b.length) {
+            result.push(b.shift());
+        }
         return result;
     }
 }
 
-var a = [8, 10, 5, 1, 4, 9, 3, 2, 6, 7, 13, 11, 12, -1, 0];
+var a = [8, 10, 5, 1, 4, 9, 3, 210, -13, Math.PI, Math.E, -25, 2, 6, 7, 13, 11, 12, -1, 0];
 
 console.log('arr: ' + a);
 console.log('mSort: ' + mSort(a));
+console.log('mSort (desc): ' + mSort(a, false));
+
