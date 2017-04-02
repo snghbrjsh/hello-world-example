0a7230e88ca4d2e16b459d802821f721f5b79d65
Brijesh Singh <snghbrjsh@gmail.com>
2017-04-03 01:22:38 +0530

    changing the directory structure

---
+++ algorithms/dutch-flag-problem.js
@@ -0,0 +1,9 @@
+/*
+* Problem: 
+* Given an array with only 3 elements - 0, 1, 2
+* Write an algorithm to rearrange them in order - all 0's togather, all 1's togather 
+* Constraint: do not use any extra space
+*/
+
+var arr = [0, 2, 1, 1, 2, 0, 2, 1, 1, 0, 0, 2, 1, 2];
+
