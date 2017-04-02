71fab5382f77634078b554cb20d6881682a12740
Brijesh Singh <snghbrjsh@gmail.com>
2017-04-02 23:25:18 +0530

    add another way to do bubble sort, (works efficiently for cases where array gets sorted in first few iteration and subsequent iterations can be skipped)

--- nodejs/algorithms/bubble-sort.js
+++ nodejs/algorithms/bubble-sort.js
@@ -1,5 +1,5 @@
 function bSort(arr, isAsc) {
-	for(var i=0; i<arr.length-1; i++) {
+	for(var i=0; i<arr.length; i++) {
 		for(var j=0; j<arr.length-i-1; j++) {
 			if(arr[j] > arr[j+1]){
 				var temp = arr[j];
@@ -11,7 +11,31 @@ function bSort(arr, isAsc) {
 	return arr;
 }
 
-var a = [8, 10, 5, 1, 4, 9, 3, 2, 6, 7, 13, 11, 12, -1, 0];
 
-console.log('arr: ' + a);
-console.log('bSort: ' + bSort(a, false));
+/*
+* if any outer iteration pass without any swap, it's likely that you don't need anymore iteration.
+* In that case array has already been sorted.
+*/
+function bSort2(arr){
+	var swapped, temp;
+	do{
+		swapped = false;
+		for(var i=0; i<arr.length; i++){
+			if(arr[i] > arr[i+1] && (typeof arr[i] === 'number') && (typeof arr[i+1] === 'number')) {
+				temp = arr[i];
+				arr[i] = arr[i+1];
+				arr[i+1] = temp;
+
+				swapped = true;
+			}
+		}
+	}while(swapped);
+
+	return arr;
+}
+
+var a = [8, 10, 5, 1, 4, 9, 3, 2, -12, -7, -12, 6, 7, 13, 11, 12, -1, 0];
+
+console.log('arr: ', a);
+console.log('bSort: ',  bSort(a.slice(), false));
+console.log('bSort2: ',  bSort2(a.slice()));
