509cc71d3d0e445fe13679991ecfbc349b14f0b4
Brijesh Singh <snghbrjsh@gmail.com>
2017-04-04 11:00:44 +0530

    add lomuto quick-sort

--- algorithms/quick-sort.js
+++ algorithms/quick-sort.js
@@ -18,8 +18,8 @@ function qSort(arr){
 }
 
 function qSortHoare(arr, low, high){
-	low = low || 0;
-	high = high || arr.length-1;
+	low = (typeof low === 'number') ? low : 0;
+	high = (typeof high === 'number') ? high : (arr.length - 1);
 	
 	var pivot = partitionHoare(arr, low, high);
 	// console.log(low, high, pivot);
@@ -56,8 +56,54 @@ function qSortHoare(arr, low, high){
 	}
 }
 
+function qSortLomuto(array, start, end){
+	start = (typeof start === 'number') ? start : 0;
+	end = (typeof end === 'number') ? end : (array.length - 1);
 
+	/*
+	* Utility function to swap elements at given index.
+	*/
+	function swap(a, i, j){
+		var temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+
+	/*
+	* Core logic
+	* take last element as the pivot value
+	* all values less than pivot should be before pivot greater than it should be after pivot.
+	*/
+	function partition(arr, start, end){
+		var pIndex = start;
+		var pivot = arr[end];
+
+		for(var i=start; i<end; i++){
+			if(arr[i] <= pivot){
+				swap(arr, pIndex, i);
+				pIndex++;
+			}
+		}
+		swap(arr, pIndex, end); // swap the last element(pivot in this case) with the first element of right sub-array
+
+		return pIndex;
+	}
+
+	/*
+	* partition the array around the pivot value,
+	* call the qSort on the resulting subarrays
+	* Base Case: 'start' index becomes equal or greater than 'end' index
+	*/
+	if(start < end){
+		var index = partition(array, start, end);
+		// console.log(start, index, end, "::::", array);
+		qSortLomuto(array, start, index-1);
+		qSortLomuto(array, index+1, end);
+	}
+	return  array;
+}
 var a = [8, 10, 5, 1, 4, 9, 3, 210, -13, -25, 2, 6, 7, 13, 11, 12, -1, 0];
 console.log('arr: ', a);
 console.log('qSort: ', qSort(a.slice()));
 console.log('qSortHoare: ', qSortHoare(a.slice()));
+console.log('qSortLomuto: ', qSortLomuto(a.slice()));
